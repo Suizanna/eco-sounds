104620f8a0f273c97f753c335ce6c7270fa3be32
Suizanna <73129581+Suizanna@users.noreply.github.com>
2022-02-07 05:43:51 +0200

    add

--- index.js
+++ index.js
@@ -2,67 +2,46 @@
 const player = document.querySelector(".player");
 const progressBar = document.querySelector(".progress-container");
 const progress = document.querySelector(".progress");
-const main = document.querySelector(".main-img");
+const main = document.querySelector(".main");
 const title = document.querySelector(".song-title");
+const navPlayList = document.querySelectorAll(".nav-item");
 
 const playBtn = document.querySelector(".play");
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
 const audio = new Audio();
 let isPlay = false;
 
 // Song titles
-const songs = ["Forest", "Solovey", "Drozd", "Zarynka", "Javoronok", "Slavka"];
+const songs = ["forest", "solovey", "drozd", "zarynka", "javoronok", "slavka"];
 
-// Default song
+// Default indx song
 let songIndx = 0;
 
 // Init
 function loadSong(song) {
-  title.innerHTML = song;
   audio.src = `assets/audio/${song}.mp3`;
   // change background img
-  //  main.src = `img/${songIndx}.jpg`;
+  //  main.src = `/img/${songIndx}.jpg`;
 }
 loadSong(songs[songIndx]);
 
-// Next
-function nextSong() {
-  if (songIndx === songs.length - 1) {
-    songIndx = 0;
-  } else {
-    songIndx++;
-  }
-  loadSong(songs[songIndx]);
-  playAudio();
-}
-// Prev
-function prevSong() {
-  //   if (songIndx < 0) {
-  if (songIndx === 0) {
-    songs.length - 1;
-  } else {
-    songIndx--;
-  }
-  loadSong(songs[songIndx]);
-  playAudio();
-}
 // Play
 function playAudio() {
-  //   audio.currentTime = 0;
+  //начинает всегда с начала
+  // audio.currentTime = 0;
   playBtn.classList.add("pause");
   audio.play();
   isPlay = true;
 }
+
 // Pause
 function pauseAudio() {
+  // audio.currentTime = 0;
   playBtn.classList.remove("pause");
-  //   audio.currentTime = 0;
   audio.pause();
   isPlay = false;
 }
 
-// Play & Pause
+// Play & Pause Listener
 playBtn.addEventListener("click", () => {
   if (!isPlay) {
     playAudio();
@@ -71,28 +50,16 @@ playBtn.addEventListener("click", () => {
   }
 });
 
-// Progress bar
-function updateProgress(e) {
-  //   const { currentTime, duration } = e.srcElement;
-  //   console.log(currentTime);
-  //   console.log(duration);
-  const percent = (audio.currentTime / audio.duration) * 100;
-  progress.style.width = `${percent}%`;
+// ChangeImage
+function changeImage(e) {
+  navPlayList.forEach((e) => e.classList.remove("active"));
+  e.target.classList.add("active");
+  // меняем img
+  main.style.backgroundImage = `url('./assets/img/${e.target.dataset.item}.jpg')`;
+  audio.src = `./assets/audio/${e.target.dataset.item}.mp3`;
+  playAudio();
 }
 
-// Set progress
-function setProgress(e) {
-  const width = this.clientWidth; //вся ширина
-  const clickX = e.offsetX; // куда кликнули
-  const duration = audio.duration;
-
-  audio.currentTime = (clickX / width) * duration;
-}
-
-// Autoplay
-audio.addEventListener("ended", nextSong);
-
-prevBtn.addEventListener("click", prevSong);
-nextBtn.addEventListener("click", nextSong);
-audio.addEventListener("timeupdate", updateProgress);
-progressBar.addEventListener("click", setProgress);
+navPlayList.forEach((el) => el.addEventListener("click", changeImage));
+// Autoplay. Когда закончится начать сначала.
+audio.addEventListener("ended", playAudio);
